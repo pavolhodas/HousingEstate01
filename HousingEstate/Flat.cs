16724709741db04f25a1b5f486e962f537205791
pavolhodas <pavolhodas4@gmail.com>
2020-11-24 15:04:28 +0100

    Add Flat.cs, new fields and constructor

---
+++ HousingEstate/Flat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingEstate
+{
+    class Flat
+    {
+        private int numberOfFlat { get; }
+        private string area { get; }
+        private int numberOfRooms { get; }
+
+        public Flat(int NumberOfFlat, string Area, int NumberOfRooms)
+        {
+            numberOfFlat = NumberOfFlat;
+            area = Area;
+            numberOfFlat = NumberOfRooms;
+        }
+    }
+}
